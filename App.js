25c2fa0c602b5ab4caf530abec23295920ad187e
omer <omerbardacrm@gmail.com>
2019-07-03 22:26:47 +0000

    add navbar

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import HomeScreen from './screens/HomeScreen';
+import NavBarDrawer from './NavBar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavBarDrawer></NavBarDrawer> 
     </View>
+    
   );
 }
 
@@ -13,8 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
- 
+  
 });
